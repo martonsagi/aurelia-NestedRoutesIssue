fb5570ca78738d3a63f7a86db3d1c04587020cb8
Lars Kemmann <lars.kemmann@bynalogic.com>
2016-11-03 17:02:38 -0400

    Set up item management viewmodel

---
+++ src/item.ts
@@ -0,0 +1,4 @@
+export class Item {
+    constructor(public id: number, public description: string) {
+    }
+}
--- src/secondary2.ts
+++ src/secondary2.ts
@@ -1,3 +1,31 @@
+// import {Router, RouterConfiguration} from 'aurelia-router';
+import {inject} from 'aurelia-framework';
+import { Item } from './item';
 
 export class Secondary2 {
+    constructor() {
+        this.items.push(new Item(1, "Item 1"));
+        this.items.push(new Item(2, "Item 2"));
+    }
+
+    //router: Router;
+    items: Item[];
+    currentItem: Item;
+
+    private nextId = 3;
+    addItem() {
+        var id = this.nextId++;
+        this.items.push(new Item(id, `Item ${id}`));
+    }
+    // selectItem(item: Item) {
+    //     this.currentItem = item;
+    // }
+
+    // configureRouter(config: RouterConfiguration, router: Router){
+    //     config.map([
+    //         { route: '',          moduleId: 'no-item',   title: 'Select an item'},
+    //         { route: 'item/:id',  moduleId: 'item-detail', name:'item' }
+    //     ]);
+    //     this.router = router;
+    // }
 }
