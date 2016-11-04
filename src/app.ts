9c2c38b703a234c5b4d2f32f2f791dfad22bf349
Lars Kemmann <lars.kemmann@bynalogic.com>
2016-11-04 18:07:35 -0400

    Finished rewriting menu code w/ manual link generation

--- src/app.ts
+++ src/app.ts
@@ -8,13 +8,13 @@ export class App {
     config.title = 'Aurelia Nested Routes Issue';
     config.map([
       { route: '', redirect: 'primary1' },
-      { route: 'primary1',  moduleId: 'primary1', name:'primary1', nav: true, title: "Primary 1" },
-      { route: 'primary2',  moduleId: 'primary2', name:'primary2', nav: true, title: "Primary 2" }
+      { route: 'primary1',  moduleId: 'primary1', name:'primary1', title: "Primary 1" },
+      { route: 'primary2',  moduleId: 'empty', name:'primary2' }
     ]);
     config.map([
       { route: 'primary2', redirect: 'primary2/secondary1' },
-      { route: 'primary2/secondary1',  moduleId: 'secondary1', name:'secondary1', nav: true, title: "Secondary 1" },
-      { route: 'primary2/secondary2',  moduleId: 'secondary2', name:'secondary2', nav: true, title: "Secondary 2" }
+      { route: 'primary2/secondary1',  moduleId: 'secondary1', name:'secondary1', title: "Secondary 1" },
+      { route: 'primary2/secondary2',  moduleId: 'secondary2', name:'secondary2', title: "Secondary 2" }
     ]);
 
     this.router = router;
--- src/primary2.ts
+++
@@ -1,5 +0,0 @@
-import {Router, RouterConfiguration} from 'aurelia-router';
-import {inject} from 'aurelia-framework';
-
-export class Primary2 {
-}
