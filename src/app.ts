c9e4380993db4a56922b5de407af220b8e5de32f
Lars Kemmann <lars.kemmann@bynalogic.com>
2016-11-04 14:00:14 -0400

    Moved secondary routes into global routes table

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,11 @@ export class App {
       { route: 'primary1',  moduleId: 'primary1', name:'primary1', nav: true, title: "Primary 1" },
       { route: 'primary2',  moduleId: 'primary2', name:'primary2', nav: true, title: "Primary 2" }
     ]);
+    config.map([
+      { route: 'primary2', redirect: 'primary2/secondary1' },
+      { route: 'primary2/secondary1',  moduleId: 'secondary1', name:'secondary1', nav: true, title: "Secondary 1" },
+      { route: 'primary2/secondary2',  moduleId: 'secondary2', name:'secondary2', nav: true, title: "Secondary 2" }
+    ]);
 
     this.router = router;
   }
--- src/primary2.ts
+++ src/primary2.ts
@@ -2,15 +2,4 @@ import {Router, RouterConfiguration} from 'aurelia-router';
 import {inject} from 'aurelia-framework';
 
 export class Primary2 {
-  router: Router;
-
-  configureRouter(config: RouterConfiguration, router: Router){
-    config.map([
-      { route: '', redirect: 'secondary1' },
-      { route: 'secondary1',  moduleId: 'secondary1', name:'secondary1', nav: true, title: "Secondary 1" },
-      { route: 'secondary2',  moduleId: 'secondary2', name:'secondary2', nav: true, title: "Secondary 2" }
-    ]);
-
-    this.router = router;
-  }
 }
